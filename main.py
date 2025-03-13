5863a3386809b7b356a132ccaf1cd3add9207576
Chelovek2343 <79435067+Chelovek2343@users.noreply.github.com>
2025-03-13 10:28:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains (you can restrict it to specific origins later)
 
 @app.route("/predict", methods=["POST"])
 def predict():
